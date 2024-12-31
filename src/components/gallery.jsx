a041ab91646a757e6bba9ef7a28a0b1a037e14e6
Ali <149102451+Ali-for-dev@users.noreply.github.com>
2024-12-31 16:26:28 +0100

    Website

---
+++ src/components/gallery.jsx
@@ -0,0 +1,38 @@
+import { Image } from "./image";
+import React from "react";
+
+export const Gallery = (props) => {
+  return (
+    <div id="portfolio" className="text-center">
+      <div className="container">
+        <div className="section-title">
+          <h2>Fields</h2>
+          <p>
+            Avenir Trading, a proud Algerian company, specializes in exporting
+            high-quality agro-food products and agronomic goods. We are
+            dedicated to sharing Algeria's rich agricultural heritage with the
+            world through excellence and sustainability.
+          </p>
+        </div>
+        <div className="row">
+          <div className="portfolio-items">
+            {props.data
+              ? props.data.map((d, i) => (
+                  <div
+                    key={`${d.title}-${i}`}
+                    className="col-sm-6 col-md-4 col-lg-4"
+                  >
+                    <Image
+                      title={d.title}
+                      largeImage={d.largeImage}
+                      smallImage={d.smallImage}
+                    />
+                  </div>
+                ))
+              : "Loading..."}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ src/components/services.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+export const Services = (props) => {
+  return (
+    <div id="services" className="text-center">
+      <div className="container">
+        <div className="section-title">
+          <h2>Our Services</h2>
+          <p>
+            Comprehensive solutions tailored to elevate your business. We
+            specialize in exporting premium Algerian products, including fresh
+            produce, artisanal goods, and natural resources, ensuring the
+            highest quality reaches international markets.
+          </p>
+        </div>
+        <div className="row">
+          {props.data
+            ? props.data.map((d, i) => (
+                <div key={`${d.name}-${i}`} className="col-md-4">
+                  {" "}
+                  <i className={d.icon}></i>
+                  <div className="service-desc">
+                    <h3>{d.name}</h3>
+                    <p>{d.text}</p>
+                  </div>
+                </div>
+              ))
+            : "loading"}
+        </div>
+      </div>
+    </div>
+  );
+};
